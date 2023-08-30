419ffd26ddbc1d07863d05adc2ac6588cf64607d
wangtao <wangtao@ai-beings.com>
2023-08-30 20:12:01 +0800

    add quickref.js

--- script/array.js
+++ script/array.js
@@ -1,6 +1,8 @@
-let frutis = ["apple", "orange", "pear"];
-console.log(frutis.at(-1));
+'use strict'
 
-for (let value of frutis) {
-    console.log(value);
+let fruits = ["apple", "orange", "pear"];
+console.log(fruits.at(-1));
+
+for (let value of fruits) {
+    console.log(`hello ${value}`);
 }
---
+++ script/quickref.js
@@ -0,0 +1,25 @@
+let x = null;
+let name = "Tammy";
+const found = false;
+
+console.log(name, found, x);
+
+let single = 'Wheres my bandit hat?';
+let double = "Wheres my bandit hat?";
+
+console.log(single, double);
+
+let number = 100;
+number += 100;
+console.log(number);
+
+let age = 30;
+// string concatenation
+console.log('Tommy is ' + age + ' years old');
+// string interpolation
+console.log(`Tommy is ${age} years old`);
+
+let count;
+console.log(count); // undefined
+
+const numberOfColumns = 10;
